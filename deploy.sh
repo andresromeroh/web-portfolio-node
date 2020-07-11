cb783008e6080c6e546a7341673d9f3afc7630cd
ANDRESROMEROH <andres.romero22@outlook.com>
2020-07-10 20:09:09 -0600

    Removed clear instructions from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,14 @@ docker stop web-portfolio-backend
 echo 'Removing stopped container...'
 docker rm --force web-portfolio-backend
 
-echo 'Removing unused images...'
+echo 'Removing unused images if any...'
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
-
-clear
 
 echo 'Building image from Dockerfile...'
 docker build -t andresromeroh/web-portfolio-backend .
 
-clear
-
 echo 'Creating new container...'
 docker run --name web-portfolio-backend -p 5000:5000 -d andresromeroh/web-portfolio-backend
+
+echo 'Done!'
+echo 'http://192.168.1.205:5000/api/v1/health'
