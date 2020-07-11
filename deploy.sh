ec1f784e890abaef6b3b6aa12d63c2fa2a0554f5
ANDRESROMEROH <andres.romero22@outlook.com>
2020-07-10 20:36:31 -0600

    Updated deploy.sh with env variables

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,11 @@ echo 'Building image from Dockerfile...'
 docker build -t andresromeroh/web-portfolio-backend .
 
 echo 'Creating new container...'
-docker run --name web-portfolio-backend -p 5000:5000 -d andresromeroh/web-portfolio-backend
+docker run \
+-e PORT='5000' \
+-e GITHUB_ACCESS_TOKEN='4960ee26f77ddeba995301fbb159ba7ca5c0e3e7' \
+-e GITHUB_API_URL='https://api.github.com/users/ANDRESROMEROH' \
+--name web-portfolio-backend -p 5000:5000 -d andresromeroh/web-portfolio-backend
 
 echo 'Done!'
 echo 'http://192.168.1.205:5000/api/v1/health'
